f5e9a4a1eff9bc3d90ab1572063e9d9e98e2331e
JuanManuelRodriguezRivera <juan.rodriguez7@catolica.edu.sv>
2023-06-01 19:45:29 -0600

    Funcionamiento basico de las vistas completo

--- proyecto/Controllers/HomeController.cs
+++ proyecto/Controllers/HomeController.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using proyecto.Models;
 using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace proyecto.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly EmpleoDBContext _EmpleoDBContext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, EmpleoDBContext EmpleoDBContext)
         {
             _logger = logger;
+            _EmpleoDBContext = EmpleoDBContext;
         }
 
         public IActionResult Index()
         {
+            var listaTrabajos = (from t in _EmpleoDBContext.trabajo select t).ToList().OrderByDescending(t => t.salario).Take(3);
+            ViewData["trabajos"] = listaTrabajos;
+
             return View();
         }
 
